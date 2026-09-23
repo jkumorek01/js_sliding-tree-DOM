6da1fab7188c38fd55370564b45f54d039a7243a
jkumorek01 <jkumorek01@gmail.com>
2026-09-23 11:29:41 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 // write code here
+
+const tree = document.querySelector('.tree');
+// const types = tree.children;
+// console.log(products);
+
+tree.addEventListener('click', (e) => {
+  const li = e.target;
+  const child = li.firstElementChild;
+
+  if (child && li.parentElement !== tree) {
+    if (child.style.display === 'none') {
+      child.style.display = 'block';
+    } else {
+      child.style.display = 'none';
+    }
+  }
+});
