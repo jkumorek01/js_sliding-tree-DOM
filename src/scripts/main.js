8a0c13b3216322fb9e58c9fc2c8b1a39862fbaef
jkumorek01 <jkumorek01@gmail.com>
2026-09-23 15:36:25 +0200

    repaired task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,20 +6,31 @@ const tree = document.querySelector('.tree');
 // const types = tree.children;
 // console.log(products);
 
+const listItems = tree.querySelectorAll('li');
+
+for (const li of listItems) {
+  const span = document.createElement('span');
+
+  span.textContent = li.firstChild.textContent.trim();
+
+  li.firstChild.replaceWith(span);
+}
+
 tree.addEventListener('click', (e) => {
   if (e.target.tagName !== 'SPAN') {
     return;
   }
-  
+
   const li = e.target.parentElement;
-  const child = li.lastElementChild;
+  const nestedUl = li.querySelector('ul');
 
+  if (!nestedUl) {
+    return;
+  }
 
-  if (child) {
-    if (child.style.display === 'none') {
-      child.style.display = '';
-    } else {
-      child.style.display = 'none';
-    }
+  if (nestedUl.style.display === 'none') {
+    nestedUl.style.display = '';
+  } else {
+    nestedUl.style.display = 'none';
   }
 });
